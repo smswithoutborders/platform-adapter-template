657d703e1b5f27cb5dad1891d935cb58344a9e96
Promise Fru <info@promisefru.com>
2025-05-08 17:13:35 +0100

    feat: initial implementation of platform adapter template.

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+This program is free software: you can redistribute it under the terms
+of the GNU General Public License, v. 3.0. If a copy of the GNU General
+Public License was not distributed with this file, see <https://www.gnu.org/licenses/>.
+"""
+
+from adapter import GmailOAuth2Adapter
+from ipc_service import AdapterIPCService
+
+
+def main():
+    # Instantiate the adapter class here (e.g., GmailOAuth2Adapter)
+    adapter = GmailOAuth2Adapter()
+    service = AdapterIPCService(adapter)
+    service.start()
+
+
+if __name__ == "__main__":
+    main()
